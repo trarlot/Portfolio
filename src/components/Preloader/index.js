7af19d9123820c3a4cc015a658187b3c419ccbd8
Tristan Arlot <tristanarlot@Mac.lan>
2024-11-29 17:21:20 +0100

    minor change

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -4,7 +4,13 @@ import Card from '../Caroussel/Card';
 import { motion } from 'framer-motion';
 import { slideUp } from './anim';
 import { useRef, useEffect } from 'react';
+import { Saira_Condensed } from 'next/font/google';
 import gsap from 'gsap';
+
+const saira_init = Saira_Condensed({
+    weight: ['900'],
+    subsets: ['latin'],
+});
 
 export default function PageComponent({ preloaderCard }) {
     const mobileRef = useRef(true);
@@ -27,7 +33,9 @@ export default function PageComponent({ preloaderCard }) {
             initial="initial"
             exit="exit"
             className={styles.preloader}>
-            <p ref={mobileRef} className={styles.mobile}>
+            <p
+                ref={mobileRef}
+                className={`${styles.mobile} ${saira_init.className}`}>
                 Use a desktop, for a better experience
             </p>
 
